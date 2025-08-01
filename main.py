6fffad4fc56131c350412dccbccb66889cd2828a
weeksl123 <weeksl@stu.otc.school.nz>
2025-08-01 21:07:17 +1200

    Edited main

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import discord
+from discord.ext import commands
+
+# Define the bot and its command prefix
+intents = discord.Intents.default()
+intents.message_content = True  # This is required to read messages
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+# Event to notify when the bot has connected
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
+# Run the bot with your token
+bot.run('YOUR_BOT_TOKEN')
